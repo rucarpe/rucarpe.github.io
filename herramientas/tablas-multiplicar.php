587a11c78ea6cc5125a65919ddde65ffd91d6c48
rucarpe <rucarpe@icloud.com>
2022-09-11 00:24:06 +0200

    actualización

--- herramientas/tablas-multiplicar.php
+++ herramientas/tablas-multiplicar.php
@@ -1,11 +1,15 @@
 <?php
 
-$numero = '8';
+$numero='8';
 
 function tablasMultiplicar ($numero) {
-	$m = 0;
-	for ($m<= 10; $m++) {
+
+    echo "<h3>Tabla de multiplicar del $numero</h3>";
+
+	for ($m=0;$m<= 10; $m++) {
         $resultado = $numero*$m;
-        return "$numero x $m = $resultado <br>";
+        echo "$numero x $m = $resultado <br>";
     }
 }
+
+tablasMultiplicar($numero);
